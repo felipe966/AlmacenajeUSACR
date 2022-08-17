a94a8cf3da3dd8c7f046b529b533389154137b25
felipe966 <felipecastil@gmail.com>
2022-08-17 16:26:32 -0600

    Agregar archivos de proyecto.

---
+++ AlmacenajeUSACR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlmacenajeUSACR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
